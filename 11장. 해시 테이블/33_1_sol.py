ad256016dfda98c812e339a534421305341d4f79
byeongchan <lbc0646@gmail.com>
2022-02-06 22:46:57 +0900

    문제 33 풀이

---
+++ 11장. 해시 테이블/33_1_sol.py
@@ -0,0 +1,33 @@
+# 17. Letter Combinations of a Phone Number
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+# A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def dfs(index, path):
+            # 끝까지 탐색하면 백트래킹
+            if len(path) == len(digits):
+                result.append(path)
+                return
+
+            # 입력값 자릿수 단위 반복
+            for i in range(index, len(digits)):
+                # 숫자에 해당하는 모든 문자열 반복
+                for j in dic[digits[i]]:
+                    dfs(i + 1, path + j)
+
+        # 예외 처리
+        if not digits:
+            return []
+
+        dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+               "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        result = []
+        dfs(0, "")
+
+        return result
