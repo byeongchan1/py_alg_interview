ac037a179784e8ed9503b5842a501fda94a3774b
byeongchan <lbc0646@gmail.com>
2022-02-08 03:15:55 +0900

    문제 38 풀이

---
+++ 11장. 해시 테이블/38_1_sol.py
@@ -0,0 +1,32 @@
+# 332. Reconstruct Itinerary
+# https://leetcode.com/problems/reconstruct-itinerary/
+
+# You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+
+# All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+
+# For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+# You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+
+import collections
+from typing import List
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = collections.defaultdict(list)
+        # 그래프 순서대로 구성
+        for a, b in sorted(tickets):
+            graph[a].append(b)
+
+        route = []
+
+        def dfs(a):
+            # 첫 번째 값을 읽어 어휘순 방문
+            while graph[a]:
+                dfs(graph[a].pop(0))
+            route.append(a)
+
+        dfs('JFK')
+        # 다시 뒤집어 어휘순 결과로
+        return route[::-1]
