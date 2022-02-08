a20f87cf407a5646370025cfc124fa8f5ed4ca68
byeongchan <lbc0646@gmail.com>
2022-02-09 07:22:09 +0900

    문제 39 풀이

---
+++ 11장. 해시 테이블/39_2_sol.py
@@ -0,0 +1,47 @@
+# 207. Course Schedule
+# https://leetcode.com/problems/course-schedule/
+
+# There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+# For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+# Return true if you can finish all courses. Otherwise, return false.
+
+import collections
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+        # 그래프 구성
+        for x, y in prerequisites:
+            graph[x].append(y)
+
+        traced = set()
+        visited = set()
+
+        def dfs(i):
+            # 순환 구조이면 False
+            if i in traced:
+                return False
+            # 이미 방문했던 노드이면 True
+            if i in visited:
+                return True
+
+            traced.add(i)
+            for y in graph[i]:
+                if not dfs(y):
+                    return False
+            # 탐색 종료 후 순환 노드 삭제
+            traced.remove(i)
+            # 탐색 종료 후 방문 노드 추가
+            visited.add(i)
+
+            return True
+
+        # 순환 구조 판별
+        for x in list(graph):
+            if not dfs(x):
+                return False
+
+        return True
