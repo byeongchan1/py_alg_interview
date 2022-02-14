9eb03d7103fad2f0b24770050735fc8cf0a71c5f
byeongchan1 <lbc0646@gmail.com>
2022-02-15 08:06:57 +0900

    문제 40 풀이

---
+++ 11장. 해시 테이블/40_1_sol.py
@@ -0,0 +1,38 @@
+# 743. Network Delay Time
+# https://leetcode.com/problems/network-delay-time/
+
+# You are given a network of n nodes, labeled from 1 to n. 
+# You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target.
+
+# We will send a signal from a given node k. 
+# Return the time it takes for all the n nodes to receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
+
+import collections
+import heapq
+from typing import List
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        graph = collections.defaultdict(list)
+        # 그래프 인접 리스트 구성
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        # 큐 변수: [(소요 시간, 정점)]
+        Q = [(0, K)]
+        dist = collections.defaultdict(int)
+
+        # 우선 순위 큐 최소값 기준으로 정점까지 최단 경로 삽입
+        while Q:
+            time, node = heapq.heappop(Q)
+            if node not in dist:
+                dist[node] = time
+                for v, w in graph[node]:
+                    alt = time + w
+                    heapq.heappush(Q, (alt, v))
+
+        # 모든 노드 최단 경로 존재 여부 판별
+        if len(dist) == N:
+            return max(dist.values())
+        return -1
