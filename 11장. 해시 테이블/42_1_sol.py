078f738a6f1f3f169eecc719697392cef78201cc
byeongchan1 <lbc0646@gmail.com>
2022-02-17 07:14:08 +0900

    문제 42 풀이

---
+++ 11장. 해시 테이블/42_1_sol.py
@@ -0,0 +1,42 @@
+# 104. Maximum Depth of Binary Tree
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+# Given the root of a binary tree, return its maximum depth.
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+import collections
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        queue = collections.deque([root])
+        depth = 0
+
+        while queue:
+            depth += 1
+            # 큐 연산 추출 노드의 자식 노드 삽입
+            for _ in range(len(queue)):
+                cur_root = queue.popleft()
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
+        # BFS 반복 횟수 == 깊이
+        return depth
