1ba7888d5e81d3610da98f929b6bfcb323d02efa
byeongchan1 <lbc0646@gmail.com>
2022-02-19 16:44:13 +0900

    문제 46 풀이

---
+++ 11장. 해시 테이블/46_1_sol.py
@@ -0,0 +1,32 @@
+# 617. Merge Two Binary Trees
+# https://leetcode.com/problems/merge-two-binary-trees/
+
+# You are given two binary trees root1 and root2.
+
+# Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+# Return the merged tree.
+
+# Note: The merging process must start from the root nodes of both trees.
+
+import collections
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 and t2:
+            node = TreeNode(t1.val + t2.val)
+            node.left = self.mergeTrees(t1.left, t2.left)
+            node.right = self.mergeTrees(t1.right, t2.right)
+
+            return node
+        else:
+            return t1 or t2
