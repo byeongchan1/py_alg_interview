4ed271b00bd9c1bd9374ea1bb992919cf8e253ff
byeongchan1 <lbc0646@gmail.com>
2022-03-03 07:20:58 +0900

    문제 51 풀이

---
+++ 11장. 해시 테이블/51_1_sol.py
@@ -0,0 +1,31 @@
+# 1038. Binary Search Tree to Greater Sum Tree
+# https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+
+# Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+# As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    val: int = 0
+
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        # 중위 순회 노드 값 누적
+        if root:
+            self.bstToGst(root.right)
+            self.val += root.val
+            root.val = self.val
+            self.bstToGst(root.left)
+
+        return root
