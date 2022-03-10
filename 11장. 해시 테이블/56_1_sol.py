56255655410551b9ca44028734c5468c42e3a705
byeongchan1 <lbc0646@gmail.com>
2022-03-10 22:46:07 +0900

    문제 56 풀이

---
+++ 11장. 해시 테이블/56_1_sol.py
@@ -0,0 +1,53 @@
+# 208. Implement Trie (Prefix Tree)
+
+# https://leetcode.com/problems/implement-trie-prefix-tree/
+
+# A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+# Implement the Trie class:
+
+# Trie() Initializes the trie object.
+# void insert(String word) Inserts the string word into the trie.
+# boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+# boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+import collections
+
+
+# 트라이의 노드
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    # 단어 삽입
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+
+    # 단어 존재 여부 판별
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return node.word
+
+    # 문자열로 시작 단어 존재 여부 판별
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return True
