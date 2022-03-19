528f2e5e4612e5415da18e42f1e0652928169503
byeongchan1 <lbc0646@gmail.com>
2022-03-19 23:58:24 +0900

    문제 58 풀이

---
+++ 11장. 해시 테이블/58_prob.py
@@ -0,0 +1,13 @@
+# 148. Sort List
+# https://leetcode.com/problems/sort-list/
+
+# Given the head of a linked list, return the list after sorting it in ascending order.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
