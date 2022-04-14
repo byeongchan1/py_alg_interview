084c654acc53aa05f6641da4fe1369c0e7dab3ba
byeongchan1 <lbc0646@gmail.com>
2022-04-14 12:41:22 +0900

    문제 61 풀이

---
+++ 11장. 해시 테이블/61_1_sol.py
@@ -0,0 +1,26 @@
+# 179. Largest Number
+# https://leetcode.com/problems/largest-number/
+
+# Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+# Since the result may be very large, so you need to return a string instead of an integer.
+
+from typing import List
+
+
+class Solution:
+    # 문제에 적합한 비교 함수
+    @staticmethod
+    def to_swap(n1: int, n2: int) -> bool:
+        return str(n1) + str(n2) < str(n2) + str(n1)
+
+    # 삽입 정렬 구현
+    def largestNumber(self, nums: List[int]) -> str:
+        i = 1
+        while i < len(nums):
+            j = i
+            while j > 0 and self.to_swap(nums[j - 1], nums[j]):
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+                j -= 1
+            i += 1
+
+        return str(int(''.join(map(str, nums))))
