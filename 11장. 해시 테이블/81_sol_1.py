d15a0ea5d75315d4a898284e8d58880746cae916
byeongchan1 <lbc0646@gmail.com>
2022-07-22 07:30:56 +0900

    문제 81 풀이

---
+++ 11장. 해시 테이블/81_sol_1.py
@@ -0,0 +1,27 @@
+# 134. Gas Station
+# https://leetcode.com/problems/gas-station/
+
+# There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+# You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+# Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+from typing import List
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        for start in range(len(gas)):
+            fuel = 0
+            for i in range(start, len(gas) + start):
+                index = i % len(gas)
+
+                can_traval = True
+                if gas[index] + fuel < cost[index]:
+                    can_traval = False
+                    break
+                else:
+                    fuel += gas[index] - cost[index]
+            if can_traval:
+                return start
+        return -1
